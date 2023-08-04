1d10ecfb7bfcb2e0149554a1d0ed0bc6bd8bdd27
tom-peter12 <tom.peter1284@gmail.com>
2023-08-04 17:53:36 +0400

    commting changes

--- ft_unset.c
+++ ft_unset.c
@@ -6,26 +6,11 @@
 /*   By: tpetros <tpetros@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 14:29:59 by tpetros           #+#    #+#             */
-/*   Updated: 2023/08/04 17:48:05 by tpetros          ###   ########.fr       */
+/*   Updated: 2023/08/04 17:53:00 by tpetros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static int	is_invalid_key(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (ft_isalpha(str[i]) || str[i] == '_' || str[i] == '!')
-			i++;
-		else
-			return (0);
-	}
-	return (i);
-}
 
 int	ft_unset(t_data *data, t_cmd *cmd)
 {
